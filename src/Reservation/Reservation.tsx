c76bbda4922dd8ae7c078dc6d35e233cca74e457
ninaTorgunakova <ninatorgunakova@gmail.com>
2022-07-28 23:23:53 +0700

    Fixed modal

--- src/Reservation/Reservation.tsx
+++ src/Reservation/Reservation.tsx
@@ -33,10 +33,25 @@ const Reservation = () => {
           <div className='form'>
               <img alt='' className='logo' src='reservation-logo.png'/>
               <div className='input-block'>
-                <input className='field' type='text' placeholder='Name'/>
-                <input className='field' type='text' placeholder='Phone'/>
-                <input className='field' type='text' placeholder='Time preference'/>
-                <input className='field' type='text' placeholder='Number of persons'/>
+                <input className='field'
+                    type='text'
+                    placeholder='Name'
+                    maxLength={30}/>
+                <input className='field'
+                    type='text'
+                    placeholder='Phone'
+                    pattern='/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im'
+                    maxLength={30}/>
+                <input className='field'
+                    type='text'
+                    onFocus={event => { event.preventDefault(); event.target.type='datetime-local' }} 
+                    placeholder='Time preference'
+                    min={new Date().toISOString().slice(0, 16)}/>
+                <input className='field'
+                    type='number'
+                    placeholder='Number of persons'
+                    max={50}
+                    min={1}/>
               </div>
               <input autoFocus type='submit' className='button' value='RESERVE NOW' onClick={openModal}/>
           </div>
